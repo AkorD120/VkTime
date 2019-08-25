8bbf2db4a09d9d9a8f40f9573f1a48bbd2089222
AkorD120 <rusl.pavlenko@yandex.ua>
2019-08-25 20:27:07 +0300

    Добавьте файлы проекта.

---
+++ ViewWPF/AuthorizationWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Navigation;
+
+namespace ViewWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationWindow.xaml
+    /// </summary>
+    public partial class AuthorizationWindow : Window
+    {
+        public AuthorizationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            GetToken.LoadCompleted += GetToken_DocumentCompleted;
+            GetToken.Navigate("https://oauth.vk.com/authorize?client_id=" + 
+                VKAPI.__APPID + "&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends&response_type=token&v=5.52");
+        }
+
+        private void GetToken_DocumentCompleted(object sender, NavigationEventArgs e)
+        {
+            if (GetToken.Source.ToString().IndexOf("access_token=") != -1)
+            {
+                GetUserToken();
+            }
+        }
+
+        private void GetUserToken()
+        {
+            char[] symbols = { '=', '&' };
+            string[] URL = GetToken.Source.ToString().Split(symbols);
+            File.WriteAllText("UserInf.txt", URL[1] + "\n");
+            File.AppendAllText("UserInf.txt", URL[5]);
+            this.Visibility = Visibility.Hidden;
+        }
+    }
+}
---
+++ ViewWPF/VKAPI.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xNet;
+using Newtonsoft.Json;
+
+namespace ViewWPF
+{
+    /// <summary>
+    /// Логика работы с VKAPI.
+    /// </summary>
+    class VKAPI
+    {
+        /// <summary>
+        /// //ID приложения.
+        /// </summary>
+        public const string APPID = "7111113";  
+        private const string VKAPIURL = "https://api.vk.com/method/";  //Ссылка для запросов
+        private string Token;  //Токен, использующийся при запросах
+
+        public VKAPI(string AccessToken)
+        {
+            Token = AccessToken;
+        }
+
+        /// <summary>
+        /// Получение заданной информации о пользователе с заданным ID.
+        /// </summary>
+        /// <param name="UserId"> Id пользователя. </param>
+        /// <param name="Fields"></param>
+        /// <returns></returns>
+        public Dictionary<string, string> GetInformation(string UserId, string[] Fields) 
+        {
+            HttpRequest GetInformation = new HttpRequest();
+            GetInformation.AddUrlParam("user_ids", UserId);
+            GetInformation.AddUrlParam("access_token", Token);
+            GetInformation.AddUrlParam("version", "5.52");
+            string Params = "";
+            foreach (string i in Fields)
+            {
+                Params += i + ",";
+            }
+            Params = Params.Remove(Params.Length - 1);
+            GetInformation.AddUrlParam("fields", Params);
+            string Result = GetInformation.Get(VKAPIURL + "users.get").ToString();
+            Result = Result.Substring(13, Result.Length - 15);
+            Dictionary<string, string> Dict = JsonConvert.DeserializeObject<Dictionary<string, string>>(Result);
+            return Dict;
+        }
+
+        public string GetCityById(string CityId)  //Перевод ID города в название
+        {
+            HttpRequest GetCityById = new HttpRequest();
+            GetCityById.AddUrlParam("city_ids", CityId);
+            GetCityById.AddUrlParam("access_token", Token);
+            GetCityById.AddUrlParam("version", "5.52");
+            string Result = GetCityById.Get(VKAPIURL + "database.getCitiesById").ToString();
+            Result = Result.Substring(13, Result.Length - 15);
+            Dictionary<string, string> Dict = JsonConvert.DeserializeObject<Dictionary<string, string>>(Result);
+            return Dict["name"];
+        }
+
+        public string GetCountryById(string CountryId)  //Перевод ID страны в название
+        {
+            HttpRequest GetCountryById = new HttpRequest();
+            GetCountryById.AddUrlParam("country_ids", CountryId);
+            GetCountryById.AddUrlParam("access_token", Token);
+            GetCountryById.AddUrlParam("version", "5.52");
+            string Result = GetCountryById.Get(VKAPIURL + "database.getCountriesById").ToString();
+            Result = Result.Substring(13, Result.Length - 15);
+            Dictionary<string, string> Dict = JsonConvert.DeserializeObject<Dictionary<string, string>>(Result);
+            return Dict["name"];
+        }
+    }
+}
